4632d390c7ad84edc1d236f6cfb10472885e37d4
edgar <edgarisaac2000@gmail.com>
2022-10-07 22:40:09 -0600

    Service Worker Test

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-;
+//;
 
 const CACHE_NAME ='v1_cache_cv',
-urlsToCache=[
+URLS=[
     './',
     './style.css',
     './script.js',
@@ -15,44 +15,53 @@ urlsToCache=[
     
 ]
 
-//Almacenar en cache durante la instalación
-self.addEventListener('install',e=>{
-    e.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache=>{
-                return cache.addAll(urlsToCache)
-                    .then(()=>self.skipWaiting())
-            })
-            .catch(err=>console.log('Fallo registro de cache',err))
-    )
-})
-
-//Activar Service Worker y buscar recursos para funcionar sin conexion
-self.addEventListener('activate',e=>{
-    const cacheWhitelist = [CACHE_NAME]
-
-    e.waitUntil(
-        caches.keys()
-            .then(cacheNames=>{
-                cacheNames.map(cacheName=>{
-                    if(cacheWhitelist.indexOf(cacheName)===-1){
-                        return caches.delete(cacheName)
-                    }
-                })
-            })
-            .then(()=>self.clients.claim())
-    )
-})
-
-//Recuperar recursos del navegador
-self.addEventListener('fetch',e=>{
-e.respondWith(
-    caches.match(e.request)
-    .then(res=>{
-        if (res) {
-            return res
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
         }
-        return fetch(e.request)
-    })
-)
-})
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  })
+  
+  // Cache resources
+  self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(URLS)
+      })
+    )
+  })
+  
+  // Delete outdated caches
+  self.addEventListener('activate', function (e) {
+    e.waitUntil(
+      caches.keys().then(function (keyList) {
+        // `keyList` contains all cache names under your username.github.io
+        // filter out ones that has this app prefix to create white list
+        var cacheWhitelist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX)
+        })
+        // add current cache name to white list
+        cacheWhitelist.push(CACHE_NAME)
+  
+        return Promise.all(keyList.map(function (key, i) {
+          if (cacheWhitelist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i] )
+            return caches.delete(keyList[i])
+          }
+        }))
+      })
+    )
+  })
